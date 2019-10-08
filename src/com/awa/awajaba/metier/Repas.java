f2b03694715b494533e0da5902c71776c3f3fe5c
nitharsan21 <nimalaluxman.nitharsan.sio@gmail.com>
2019-10-08 16:13:38 +0200

    Implémentation du constructeur

--- src/com/awa/awajaba/metier/Repas.java
+++ src/com/awa/awajaba/metier/Repas.java
@@ -16,6 +16,9 @@ public class Repas {
     private int numero ;
     private LocalDate date ;
     
-    
+    public Repas (int numero,LocalDate date){
+		this.numero= numero;
+		this.date=date;
+	}
     
 }
